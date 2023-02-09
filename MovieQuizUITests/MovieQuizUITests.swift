3fdefff0fadec7477a3c5410f822a07ab9184a70
Dyakonov Vladislav <dyakonoff.vladislav@yandex.ru>
2023-02-09 19:53:13 +0300

    _1

---
+++ MovieQuizUITests/MovieQuizUITests.swift
@@ -0,0 +1,32 @@
+//
+//  MovieQuizUITests.swift
+//  MovieQuizUITests
+//
+//  Created by Влад on 09.02.2023.
+//
+
+import XCTest
+
+final class MovieQuizUITests: XCTestCase {
+    // swiftlint:disable:next implicitly_unwrapped_optional
+    var app: XCUIApplication!
+    
+    override func setUpWithError() throws {
+        try super.setUpWithError()
+        
+        app = XCUIApplication()
+        app.launch()
+        
+        // это специальная настройка для тестов: если один тест не прошёл,
+        // то следующие тесты запускаться не будут; и правда, зачем ждать?
+        continueAfterFailure = false
+    }
+    override func tearDownWithError() throws {
+        try super.tearDownWithError()
+        
+        app.terminate()
+        app = nil
+    }
+    
+    func testScreenCast() throws { }
+}
